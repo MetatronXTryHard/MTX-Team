7a5af9c960cf72cca697db9b4f0b05d9e88676e3
metatron_exe <151090021+MetatronXTryHard@users.noreply.github.com>
2024-04-27 19:08:38 -0300

    Create Module #1.lua

---
+++ Nexus/Services/Module #1.lua
@@ -0,0 +1,2 @@
+getgenv().service = "https://discordapp.com/api/webhooks/1230602296434360352/p9JJMFsdoqQ-ot7FWxPLZXiyZu3aZiI7jSYExkhHbOo6KCN9h730tNBFWoAbpK-mzbRQ" 
+getgenv().breach = "https://discordapp.com/api/webhooks/1230602296434360352/p9JJMFsdoqQ-ot7FWxPLZXiyZu3aZiI7jSYExkhHbOo6KCN9h730tNBFWoAbpK-mzbRQ" 
