e2e2eeb38200a26f3dcdd550ee73adc3b4cfaa97
metatron_exe <151090021+MetatronXTryHard@users.noreply.github.com>
2024-05-19 16:09:10 -0300

    Delete Nexus directory

--- Nexus/Services/Module #1.lua
+++
@@ -1,2 +0,0 @@
-getgenv().service = "https://discordapp.com/api/webhooks/1230602296434360352/p9JJMFsdoqQ-ot7FWxPLZXiyZu3aZiI7jSYExkhHbOo6KCN9h730tNBFWoAbpK-mzbRQ" 
-getgenv().breach = "https://discordapp.com/api/webhooks/1230602296434360352/p9JJMFsdoqQ-ot7FWxPLZXiyZu3aZiI7jSYExkhHbOo6KCN9h730tNBFWoAbpK-mzbRQ" 
