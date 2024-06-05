5f4294031c79f92671169c84faeb6a513f39c593
metatron_exe <151090021+MetatronXTryHard@users.noreply.github.com>
2024-06-05 14:41:22 -0300

    Update Version.lua

--- Modules/Whitelist/Version.lua
+++ Modules/Whitelist/Version.lua
@@ -1,4 +1,14 @@
 getgenv().Info = {
   ["Version"] = {Loader = {"0.1"}
 }
+local function printLoaderVersion()
+    if getgenv().Info and getgenv().Info.Version and getgenv().Info.Version.Loader then
+        local version = getgenv().Info.Version.Loader[1]
+        print("Loader Version: " .. version)
+    else
+        print("Loader Version information not found.")
+    end
+end
+  
+printLoaderVersion()
 return VersionData
