c1c806352526f4e0e73611d97bbd9d902ba926d9
metatron_exe <151090021+MetatronXTryHard@users.noreply.github.com>
2024-06-05 15:03:15 -0300

    Update Version.lua

--- Modules/Whitelist/Version.lua
+++ Modules/Whitelist/Version.lua
@@ -4,17 +4,4 @@ if not game:IsLoaded() then
 end
 
 ---[[ Loader Info ]]---
-getgenv().Info = {
-  ["Version"] = {Loader = {"0.1"}}
-}
-
----[[ Loader Version Check ]]---
-local function VersionCheck()
-    if getgenv().Info and getgenv().Info.Version and getgenv().Info.Version.Loader then
-        local version = getgenv().Info.Version.Loader[1]
-        print("Loader Version: " .. version)
-    else
-        print("Loader Version information not found.")
-    end
-end
-VersionCheck()
+getgenv().Info = {["Version"] = {Loader = {"0.1"}}}
