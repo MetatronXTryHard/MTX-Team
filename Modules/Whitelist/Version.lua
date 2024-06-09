a22e8d51fad2d028d58656fa31e007c0b096352c
metatron_exe <151090021+MetatronXTryHard@users.noreply.github.com>
2024-06-09 02:18:42 -0300

    Update Version.lua

--- Modules/Whitelist/Version.lua
+++ Modules/Whitelist/Version.lua
@@ -1,10 +1,3 @@
----[[ Loader Info ]]---
-getgenv().Info = {
-    ["Version"] = {
-        Loader = {"0.1"}
-    }
-}
-
----[[ Variables ]]---
-local WL = getgenv().Info.Version.Loader[1]
-print(WL)
+--[[ Loader Info ]]--
+getgenv().Info = {["Version"] = {Loader = {"0.1"}}}
+---------------------
